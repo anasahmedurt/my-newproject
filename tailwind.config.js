0e1cfd363ce6ad81f6c62d1c05741880553297ad
anas-ahmed <anasahmed.in30@gmail.com>
2023-11-03 22:53:48 +0600

    color change

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container:{
+      margin:'200px',
+      padding:'1rem',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
